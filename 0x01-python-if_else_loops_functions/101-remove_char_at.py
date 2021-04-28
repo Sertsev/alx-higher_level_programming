e9d0a9915ca45068125a1df044ee1e7c8904e217
sertsev <sertseshewa@gmail.com>
2021-04-28 23:57:26 +0300

    fixing indentations

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,5 @@
 def remove_char_at(str, n)
-    for i in range(len(str:))
-        if i == n
-        str[n] = ""
+    if n >= 0:
+        return str[:n] + str[n + 1:]
+    else:
+        return str
