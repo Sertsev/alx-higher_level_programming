ab62ecc56912f0227cab054a0e21e56b37767d18
sertsev <sertseshewa@gmail.com>
2021-05-05 22:05:36 +0300

    starting with modules assignments

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+
+
+def add(a, b):
+    print("{:d} + {:d} = {:d}".format(a, b, (a + b)))
