dcb1a4aa72de7aa93ae209036985de273c9210df
sertsev <sertseshewa@gmail.com>
2021-09-17 22:01:48 +0300

    minimizing lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    _matrix = [[]] * len(matrix)
-    if matrix == []:
-        return 0
-    _matrix = list(map(lambda x: (list(map(lambda x: x*x, x))), matrix))
-    return _matrix
+    if matrix == []: return 0
+    return list(map(lambda x: (list(map(lambda x: x*x, x))), matrix))
