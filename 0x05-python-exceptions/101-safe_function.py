f418ca363ec25a1eac581510a22aeea698ee397b
sertsev <sertseshewa@gmail.com>
2021-10-01 19:52:34 +0300

    Advanced tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,9 +7,3 @@ def safe_function(fct, *args):
     except Exception as err:
         print("Exception:", err)
         return None
-
-def print_message():
-    print("Hello")
-
-result = safe_function(print_message)
-print(result)
