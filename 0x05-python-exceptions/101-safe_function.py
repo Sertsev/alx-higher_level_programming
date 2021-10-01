2ad0bf7a4bbe5ceb2de62cbfcee2b3530c07d0c0
sertsev <sertseshewa@gmail.com>
2021-10-01 19:49:40 +0300

    Advanced tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
     except Exception as err:
-        print("Exception:",err)
+        print("Exception:", err)
         return None
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,1 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b):
-    
