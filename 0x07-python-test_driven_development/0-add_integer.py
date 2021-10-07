fbc05291e505738eb769145edff8c5d4e48e3d03
Sertsedengle Shewandagn <sertseshewa@gmail.com>
2021-10-07 09:35:20 +0300

    finishing mandatory tasks

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,10 +3,12 @@ def add_integer(a, b=98):
     """My addition function
     Args:
         a: first integer
-        b: second integer
+        b: second integer which have a default value 98
     Returns:
         The return value. a + b
     """
+
+
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(b) != int and type(b) != float:
