2a5e0711cf99403266f1eca7052ce4c5080bab56
sertsev <sertseshewa@gmail.com>
2021-10-23 10:55:51 +0300

    trying to fix stupid module document error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-square0 = __import__("0-square").Square
 """ a class to define a size of square """
 
 
 class Square(square0):
-    """ an inherited class from Square definition class """
+    """ an inherited class from Square definition class
+    Args:
+        square0 (class): a class which defin a square
+    """
+
     def __init__(self, size):
-        """initialization function"""
+        """initialization function
+        Args:
+            size (integer): a size attribute passed
+        """
         self.__size = size
