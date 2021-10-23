c998a0a0032bb9d3a9432fb7a0fccc098bac9fa6
sertsev <sertseshewa@gmail.com>
2021-10-23 10:53:26 +0300

    a class that defines a square with size by inheriting from the previous task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,8 +5,6 @@ square0 = __import__("0-square").Square
 
 class Square(square0):
     """ an inherited class from Square definition class """
-
     def __init__(self, size):
         """initialization function"""
-        
         self.__size = size
